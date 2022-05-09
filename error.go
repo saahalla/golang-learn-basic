bb1606385122b9a0378243342e4a48c261d9a4a1
Sahal M <sahalmahfudh07@gmail.com>
2022-05-09 21:04:40 +0700

    tutorial 38 error interface

---
+++ error.go
@@ -0,0 +1,10 @@
+package main
+
+/**
+error, bisa di pelajari lagi, intinya sudah ada interface sendiri
+biasanya error digunakan pada return function yg memiliki error,
+nanti return function tersebut diberikan (result, err)
+*/
+func main() {
+
+}
--- interfaces.go
+++ interfaces.go
@@ -1,8 +1,31 @@
 package main
 
+import "fmt"
+
+type HasName interface {
+	GetName() string
+}
+
+func SayHello(hasName HasName) {
+	fmt.Println("Hello", hasName.GetName())
+}
+
+type Person struct {
+	Name string
+}
+
+func (person Person) GetName() string {
+	return person.Name
+}
+
 func main() {
 	/**
 	Mohon pelajari lagi ya :)
 	yang saya paham, interface bisa menerapkan methode di dalam struct tapi tidak langsung seperti class, bikin struct function baru terlebih dahulu
+	- type data abstract
 	*/
+	var sahal Person
+	sahal.Name = "sahal"
+	SayHello(sahal)
+
 }
